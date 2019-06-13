fc38dea689ce91c5c4968a97f890849b26e25a35
Helena-herasymiuk <he.lena280299@gmail.com>
2019-06-13 15:04:20 +0300

    Updates

---
+++ precache-manifest.7a7f7d6412dec6312cd9cd8a573e64c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b84ce3df296377907c631c4b3abafa3",
+    "url": "/pcr-demo/index.html"
+  },
+  {
+    "revision": "f82a40869061c17120cb",
+    "url": "/pcr-demo/static/css/main.5f7d535e.chunk.css"
+  },
+  {
+    "revision": "3ca45c5dc8956fc13d01",
+    "url": "/pcr-demo/static/js/2.a1e46d97.chunk.js"
+  },
+  {
+    "revision": "f82a40869061c17120cb",
+    "url": "/pcr-demo/static/js/main.65a9bdcb.chunk.js"
+  },
+  {
+    "revision": "7e123fa5691f6cf5289a",
+    "url": "/pcr-demo/static/js/runtime~main.c58dc844.js"
+  }
+]);
